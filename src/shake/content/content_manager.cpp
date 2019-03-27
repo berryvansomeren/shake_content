a1d5c88fed27703d077d503f79a2dc61c2d32211
BerryVanSomeren <berryvansomeren@gmail.com>
2019-03-27 17:02:20 +0100

    fixed indentation

--- src/shake/content/content_manager.cpp
+++ src/shake/content/content_manager.cpp
@@ -3,14 +3,14 @@
 namespace shake {
 namespace content {
 
-    void init_gl( const GLLoadProc& gl_load_proc )
+void init_gl( const GLLoadProc& gl_load_proc )
+{
+    const auto load_success = gladLoadGLLoader( ( GLADloadproc ) gl_load_proc );
+    if ( !load_success ) 
     {
-        const auto load_success = gladLoadGLLoader( ( GLADloadproc ) gl_load_proc );
-        if ( !load_success ) 
-        {
-            CHECK_FAIL( "Could not initialize OpenGl Context" );
-        }
+        CHECK_FAIL( "Could not initialize OpenGl Context" );
     }
+}
 
-}
-}
+} // namespace content
+} // namespace shake
